573fc0892eb35fe34f5a6952bbc6fa29719b2c8d
Nick Barone <nick@nbar1.com>
2013-11-21 00:31:05 -0500

    inital commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+<head>
+	<title>Nick Barone</title>
+	<meta name="title" content="Nick Barone - nbar1.com" />
+	<meta name="description" content="nick barone, nbar1, xnickbarone, sylvester barone" />
+	<meta name="keywords" content="The personal website of software engineer Nick Barone." />
+	<meta name="author" content="Nick Barone" />
+	<link rel="stylesheet" type="text/stylesheet" href="/assets/stylesheets/global.css" />
+</head>
+<body>
+	<header>
+		<nav>
+			<label for="navigation_intent_1" rel="what_i_do">what i do</label>
+			<label for="navigation_intent_2" rel="where_i_am">where i am</label>
+		</nav>
+		<div id="navigation_intents">
+		</div>
+	</header>
+	<article id="title">&nbsp;Nick Barone</article>
+	<article id="pages">
+		<input type="radio" name="navigation_intent" id="navigation_intent_1" checked />
+		<input type="radio" name="navigation_intent" id="navigation_intent_2" />
+		<section id="what_i_do">
+			<ul>
+				<li>I am a programmer specializing in both front-end and back-end architecture.</li>
+				<li>I use a lot of fancy things like <mark>css3</mark>, <mark>html5</mark> and <mark>JavaScript</mark>.</li>
+				<li>I use a lot of other fancy things you can't really see from here.</li>
+				<li>Things like <mark>PHP</mark>, <mark>Python</mark> and <mark>Node.js</mark>.</li>
+				<li>I like things like object-oriented design patterns and agile practices.</li>
+				<li>I really like where the web is going, and css3 in particular.</li>
+				<li>In fact, this whole site is css3, no JavaScript at all!.</li>
+			</ul>
+		</section>
+		<section id="where_i_am">
+			<ul>
+				<li><a href="http://github.com/nbar1" target="_blank">github</a></li>
+				<li><a href="http://twitter.com/nbar1" target="_blank">twitter</a></li>
+				<li><a href="http://www.linkedin.com/profile/view?id=57626985" target="_blank">linkedin</a></li>
+			</ul>
+		</section>
+	</article>
+</body>
+</html>
